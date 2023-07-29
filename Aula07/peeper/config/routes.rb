a09000f947debf44301dc85ccda1de7856b4323a
SSantos425 <samuel.tri@hotmail.com>
2023-07-29 05:13:16 -0300

    controller and view for User model + seeds

--- Aula07/peeper/config/routes.rb
+++ Aula07/peeper/config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  get '/users', to: 'users#index'
+  
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/:id', to: 'users#show', as: 'user'
+  post '/users', to: 'users#create'
+
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to:'users#destroy', as: 'destroy_user'
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
--- Aula07/peeper/db/seeds.rb
+++ Aula07/peeper/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+sam = User.create(handle: "sam", display_name: "samuel",bio:"likes DC",born_at:1998-06-20)
+gabi = User.create(handle: "gabi",display_name: "Shingeki",bio:"Dont like the potato girl",born_at:1998-06-20)
+james = User.create(handle: "james",display_name: "watson",bio:"sidekick of sherlock",born_at:1998-06-20)
